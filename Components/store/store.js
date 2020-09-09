31120932524f0ac0ab83a3baae6a4d2f7a03976a
Danish <sheikhmdanish.001@gmail.com>
2020-09-09 11:39:49 +0530

    Used reducer insted of useState() to manage the states

--- Components/store/store.js
+++ Components/store/store.js
@@ -1,8 +1,10 @@
 import { createStore, combineReducers } from 'redux'
 import { reducer as form } from 'redux-form'
+import reviewReducer from '../store'
 
 const reducers = combineReducers({
   form,
+  reviewReducer,
 })
 
 export default createStore(reducers)
